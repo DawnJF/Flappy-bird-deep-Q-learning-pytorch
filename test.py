56e7142870591dec213b3fe280f66c4e85e82527
Jianfei Ma <majianfei1001@gmail.com>
2025-01-27 12:12:52 +0800

    add net 2

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 """
 @author: Viet Nguyen <nhviet1009@gmail.com>
 """
+
 import argparse
 import torch
 
@@ -11,9 +12,17 @@ from src.utils import pre_processing
 
 def get_args():
     parser = argparse.ArgumentParser(
-        """Implementation of Deep Q Network to play Flappy Bird""")
-    parser.add_argument("--image_size", type=int, default=84, help="The common width and height for all images")
-    parser.add_argument("--saved_path", type=str, default="trained_models")
+        """Implementation of Deep Q Network to play Flappy Bird"""
+    )
+    parser.add_argument(
+        "--image_size",
+        type=int,
+        default=84,
+        help="The common width and height for all images",
+    )
+    parser.add_argument(
+        "--check_points", type=str, default="trained_models/flappy_bird_1000000"
+    )
 
     args = parser.parse_args()
     return args
@@ -24,16 +33,20 @@ def test(opt):
         torch.cuda.manual_seed(123)
     else:
         torch.manual_seed(123)
-    file = "{}/flappy_bird".format(opt.saved_path)
-    print(f"load: {file}")
+
+    print(f"load: {opt.check_points}")
     if torch.cuda.is_available():
-        model = torch.load("{}/flappy_bird".format(opt.saved_path))
+        model = torch.load(opt.check_points)
     else:
-        model = torch.load("{}/flappy_bird".format(opt.saved_path), map_location=lambda storage, loc: storage)
+        model = torch.load(opt.check_points, map_location=lambda storage, loc: storage)
     model.eval()
     game_state = FlappyBird()
     image, reward, terminal = game_state.next_frame(0)
-    image = pre_processing(image[:game_state.screen_width, :int(game_state.base_y)], opt.image_size, opt.image_size)
+    image = pre_processing(
+        image[: game_state.screen_width, : int(game_state.base_y)],
+        opt.image_size,
+        opt.image_size,
+    )
     image = torch.from_numpy(image)
     if torch.cuda.is_available():
         model.cuda()
@@ -46,8 +59,11 @@ def test(opt):
         action = torch.argmax(prediction).item()
 
         next_image, reward, terminal = game_state.next_frame(action)
-        next_image = pre_processing(next_image[:game_state.screen_width, :int(game_state.base_y)], opt.image_size,
-                                    opt.image_size)
+        next_image = pre_processing(
+            next_image[: game_state.screen_width, : int(game_state.base_y)],
+            opt.image_size,
+            opt.image_size,
+        )
         next_image = torch.from_numpy(next_image)
         if torch.cuda.is_available():
             next_image = next_image.cuda()
