29ef55e5ed8dd3cec7503b63faa93f5170fddc5a
Jianfei Ma <majianfei1001@gmail.com>
2025-10-24 11:22:35 +0800

    refactor sac II

--- src/sac_policy/low_dimensional.py
+++ src/sac_policy/low_dimensional.py
@@ -7,19 +7,31 @@ import torch.nn.functional as F
 class SoftQNetwork(nn.Module):
     def __init__(self, observation_space, action_space):
         super().__init__()
-        self.fc1 = nn.Linear(
-            np.array(observation_space.shape).prod() + np.prod(action_space.shape),
-            256,
+        input_dim = np.array(observation_space.shape).prod() + np.prod(
+            action_space.shape
         )
-        self.fc2 = nn.Linear(256, 256)
-        self.fc3 = nn.Linear(256, 1)
+
+        self.critic1 = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1),
+        )
+        self.critic2 = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1),
+        )
 
     def forward(self, x, a):
         x = torch.cat([x, a], 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+
+        q1 = self.critic1(x)
+        q2 = self.critic2(x)
+        return torch.cat([q1, q2], dim=1)
 
 
 LOG_STD_MAX = 2
@@ -69,6 +81,6 @@ class Actor(nn.Module):
         log_prob = normal.log_prob(x_t)
         # Enforcing Action Bound
         log_prob -= torch.log(self.action_scale * (1 - y_t.pow(2)) + 1e-6)
-        log_prob = log_prob.sum(1, keepdim=True)
+        log_prob = log_prob.sum(1)
         mean = torch.tanh(mean) * self.action_scale + self.action_bias
         return action, log_prob, mean
--- train/sac.py
+++ train/sac.py
@@ -18,30 +18,25 @@ from src.sac_policy.low_dimensional import Actor, SoftQNetwork
 from src.buffers import ReplayBuffer
 from src.utils import get_device, setup_logging
 
-# from src.flappy_bird_env import FlappyBirdEnv
-
 
 @dataclass
 class Args:
     exp_name: str = os.path.basename(__file__)[: -len(".py")]
     """the name of this experiment"""
     output_dir: str = "outputs/sac"
-    seed: int = 1
-    """seed of the experiment"""
-
-    track: bool = False
-    """if toggled, this experiment will be tracked with Weights and Biases"""
-
-    capture_video: bool = False
-    """whether to capture videos of the agent performances (check out `videos` folder)"""
+    render: bool = False
 
     # Algorithm specific arguments
     env_id: str = "Hopper-v4"
     """the environment id of the task"""
+    load_checkpoint: str = None
+    """path to load checkpoint from"""
     learning_starts: int = 5000
     """timestep to start learning"""
-    total_timesteps: int = 1000000
+    total_timesteps: int = 100000
     """total timesteps of the experiments"""
+    save_freq: int = 30000
+    """frequency to save checkpoints"""
 
     buffer_size: int = int(1e6)
     """the replay memory buffer size"""
@@ -64,37 +59,21 @@ class Args:
     autotune: bool = True
     """automatic tuning of the entropy coefficient"""
 
-    # Checkpoint arguments
-    save_freq: int = 200000
-    """frequency to save checkpoints"""
-    load_checkpoint: str = None
-    """path to load checkpoint from"""
-    render: bool = False
 
-
-device = get_device()
-
-
-def make_env(env_id, capture_video, render=False):
-    if capture_video:
-        env = gym.make(env_id, render_mode="rgb_array")
-        env = gym.wrappers.RecordVideo(env, f"videos/{env_id}")
-    else:
-        env = gym.make(env_id, render_mode="human" if render else None)
-    # env = SparseHopper(env)
+def make_env(env_id):
+    env = gym.make(env_id)
     env = gym.wrappers.RecordEpisodeStatistics(env)
     return env
 
 
-def save_checkpoint(actor, qf1, qf2, actor_optimizer, q_optimizer, global_step, args):
+def save_checkpoint(actor, qf, actor_optimizer, q_optimizer, global_step, args):
     """Save model checkpoint"""
     checkpoint_path = args.output_dir
 
     checkpoint = {
         "global_step": global_step,
         "actor_state_dict": actor.state_dict(),
-        "qf1_state_dict": qf1.state_dict(),
-        "qf2_state_dict": qf2.state_dict(),
+        "qf_state_dict": qf.state_dict(),
         "actor_optimizer_state_dict": actor_optimizer.state_dict(),
         "q_optimizer_state_dict": q_optimizer.state_dict(),
         "args": args,
@@ -121,10 +100,12 @@ def evaluate_agent(load_checkpoint_path, num_episodes=5, max_steps=4000):
     """Evaluate the agent and optionally render the environment"""
 
     env = make_env("Hopper-v4", False, render=True)
-    print(f"action_space: {env.action_space}")
-    print(f"observation_space: {env.observation_space}")
+    action_space = env.action_space
+    observation_space = env.observation_space
+    print(f"action_space: {action_space}")
+    print(f"observation_space: {observation_space}")
 
-    actor = Actor(env.observation_space, env.action_space)
+    actor = Actor(observation_space, action_space)
 
     load_checkpoint(load_checkpoint_path, actor, device="cpu")
 
@@ -181,7 +162,7 @@ def train():
 
     args = tyro.cli(Args)
     time_str = time.strftime("%Y-%m-%d-%H-%M-%S")
-    run_name = f"{args.env_id}__{args.exp_name}__{args.seed}__{time_str}"
+    run_name = f"{args.env_id}__{args.exp_name}__{time_str}"
     args.output_dir = os.path.join(args.output_dir, run_name)
     os.makedirs(args.output_dir, exist_ok=True)
 
@@ -194,36 +175,33 @@ def train():
         % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
     )
 
-    # TRY NOT TO MODIFY: seeding
-    random.seed(args.seed)
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = True
+
+    device = torch.device("cuda" if torch.cuda.is_available() and args.cuda else "cpu")
 
     # env setup
-    env = make_env(args.env_id, args.capture_video, render=args.render)
+    env = make_env(args.env_id)
 
-    logging.info(f"observation_space: {env.observation_space}")
-    logging.info(f"action_space: {env.action_space}")
-    logging.info(f"=" * 50)
+    action_space = env.action_space
+    observation_space = env.observation_space
 
-    actor = Actor(env.observation_space, env.action_space).to(device)
-    qf1 = SoftQNetwork(env.observation_space, env.action_space).to(device)
-    qf2 = SoftQNetwork(env.observation_space, env.action_space).to(device)
-    qf1_target = SoftQNetwork(env.observation_space, env.action_space).to(device)
-    qf2_target = SoftQNetwork(env.observation_space, env.action_space).to(device)
+    logging.info("=" * 40)
+    logging.info(f"action_space: {action_space}")
+    logging.info(f"observation_space: {observation_space}")
+    logging.info("=" * 40)
 
-    qf1_target.load_state_dict(qf1.state_dict())
-    qf2_target.load_state_dict(qf2.state_dict())
-    q_optimizer = optim.Adam(
-        list(qf1.parameters()) + list(qf2.parameters()), lr=args.q_lr
-    )
+    actor = Actor(observation_space, action_space).to(device)
+    qf = SoftQNetwork(observation_space, action_space).to(device)
+
+    qf_target = SoftQNetwork(observation_space, action_space).to(device)
+    qf_target.load_state_dict(qf.state_dict())
+    q_optimizer = optim.Adam(list(qf.parameters()), lr=args.q_lr)
     actor_optimizer = optim.Adam(list(actor.parameters()), lr=args.policy_lr)
 
     # Automatic entropy tuning
     if args.autotune:
-        target_entropy = -torch.prod(
-            torch.Tensor(env.action_space.shape).to(device)
-        ).item()
+        target_entropy = -torch.prod(torch.Tensor(action_space.shape).to(device)).item()
         log_alpha = torch.zeros(1, requires_grad=True, device=device)
         alpha = log_alpha.exp().item()
         a_optimizer = optim.Adam([log_alpha], lr=args.q_lr)
@@ -231,19 +209,20 @@ def train():
     else:
         alpha = args.alpha
 
-    env.observation_space.dtype = np.float32
+    observation_space.dtype = np.float32
     rb = ReplayBuffer(
         args.buffer_size,
-        env.observation_space,
-        env.action_space,
+        observation_space,
+        action_space,
         device,
+        n_envs=1,
         handle_timeout_termination=False,
     )
     start_time = time.time()
-    log_buffer_full = False
 
     # TRY NOT TO MODIFY: start the game
-    obs, _ = env.reset(seed=args.seed)
+    obs, _ = env.reset()
+
     for global_step in range(args.total_timesteps):
 
         if global_step == args.learning_starts:
@@ -251,144 +230,132 @@ def train():
 
         # ALGO LOGIC: put action logic here
         if global_step < args.learning_starts:
-            action = np.array(env.action_space.sample())
+            action_sample = action_space.sample()
+            actions = np.array([action_sample])
         else:
-            action, _, _ = actor.get_action(torch.Tensor(obs).unsqueeze(0).to(device))
-            action = action.detach().cpu().numpy().squeeze()
+            actions, _, _ = actor.get_action(torch.Tensor(obs).unsqueeze(0).to(device))
+            actions = actions.detach().cpu().numpy()
 
-        # TRY NOT TO MODIFY: execute the game and log data.
-        next_obs, reward, termination, truncation, info = env.step(action)
+        # execute the game
+        action_to_step = actions.squeeze() if len(actions.shape) > 1 else actions
+        next_obs, reward, terminated, truncated, info = env.step(action_to_step)
 
         # TRY NOT TO MODIFY: record reward for plotting purposes
-        if "final_info" in info:
-            for info in info["final_info"]:
-                if info is not None:
-                    # logging.info(
-                    #     f"global_step={global_step}, episodic_return={info['episode']['r']}"
-                    # )
-                    writer.add_scalar(
-                        "charts/episodic_return", info["episode"]["r"], global_step
-                    )
-                    writer.add_scalar(
-                        "charts/episodic_length", info["episode"]["l"], global_step
-                    )
-                    break
+        if terminated or truncated:
+            if "episode" in info:
+                returns = info["episode"]["r"]
+                length = info["episode"]["l"]
+                logging.info(
+                    f"global_step={global_step}, returns={returns:.2f}, length={length}"
+                )
+                writer.add_scalar("charts/episodic_return", returns, global_step)
+                writer.add_scalar("charts/episodic_length", length, global_step)
 
-        # TRY NOT TO MODIFY: save data to replay buffer; handle `final_observation`
-        real_next_obs = next_obs.copy()
-        if truncation and "final_observation" in info:  # Note
-            real_next_obs = info["final_observation"]
-        rb.add(obs, real_next_obs, action, reward, termination, info)
+        # 确保数据格式正确用于单环境
+        rb.add(
+            obs.reshape(1, -1),
+            next_obs.reshape(1, -1),
+            actions.reshape(1, -1),
+            np.array([reward]).astype(np.float32),
+            np.array([terminated]).astype(np.float32),
+            [info],
+        )
 
-        if rb.size == args.buffer_size and not log_buffer_full:
-            logging.info("Replay buffer is full!")
-            log_buffer_full = True
-
-        # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
-        obs = next_obs
+        if terminated or truncated:
+            obs, _ = env.reset()
+        else:
+            obs = next_obs
 
         # ALGO LOGIC: training.
-        if global_step > args.learning_starts:
-            data = rb.sample(args.batch_size)
-            with torch.no_grad():
-                next_state_actions, next_state_log_pi, _ = actor.get_action(
-                    data.next_observations
-                )
-                qf1_next_target = qf1_target(data.next_observations, next_state_actions)
-                qf2_next_target = qf2_target(data.next_observations, next_state_actions)
-                min_qf_next_target = (
-                    torch.min(qf1_next_target, qf2_next_target)
-                    - alpha * next_state_log_pi
-                )
-                next_q_value = data.rewards.flatten() + (
-                    1 - data.dones.flatten()
-                ) * args.gamma * (min_qf_next_target).view(-1)
+        if global_step < args.learning_starts:
+            continue
+        data = rb.sample(args.batch_size)
+        with torch.no_grad():
+            next_state_actions, next_state_log_pi, _ = actor.get_action(
+                data.next_observations
+            )
+            # [B, 2]
+            qf_next_target = qf_target(data.next_observations, next_state_actions)
+            min_qf_next_target = qf_next_target.min(dim=1)[0]  # [B]
 
-            qf1_a_values = qf1(data.observations, data.actions).view(-1)
-            qf2_a_values = qf2(data.observations, data.actions).view(-1)
-            qf1_loss = F.mse_loss(qf1_a_values, next_q_value)
-            qf2_loss = F.mse_loss(qf2_a_values, next_q_value)
-            qf_loss = qf1_loss + qf2_loss
+            min_qf_next_target = min_qf_next_target - alpha * next_state_log_pi
+            next_q_value = data.rewards.flatten() + (
+                1 - data.dones.flatten()
+            ) * args.gamma * (min_qf_next_target).view(-1)
 
-            # optimize the model
-            q_optimizer.zero_grad()
-            qf_loss.backward()
-            q_optimizer.step()
+        qf_a_values = qf(data.observations, data.actions)
+        qf1_loss = F.mse_loss(qf_a_values[:, 0], next_q_value)
+        qf2_loss = F.mse_loss(qf_a_values[:, 1], next_q_value)
+        qf_loss = qf1_loss + qf2_loss
 
-            if global_step % args.policy_frequency == 0:  # TD 3 Delayed update support
-                for _ in range(
-                    args.policy_frequency
-                ):  # compensate for the delay by doing 'actor_update_interval' instead of 1
-                    pi, log_pi, _ = actor.get_action(data.observations)
-                    qf1_pi = qf1(data.observations, pi)
-                    qf2_pi = qf2(data.observations, pi)
-                    min_qf_pi = torch.min(qf1_pi, qf2_pi)
-                    actor_loss = ((alpha * log_pi) - min_qf_pi).mean()
+        # optimize the model
+        q_optimizer.zero_grad()
+        qf_loss.backward()
+        q_optimizer.step()
 
-                    actor_optimizer.zero_grad()
-                    actor_loss.backward()
-                    actor_optimizer.step()
+        if global_step % args.policy_frequency == 0:  # TD 3 Delayed update support
+            for _ in range(
+                args.policy_frequency
+            ):  # compensate for the delay by doing 'actor_update_interval' instead of 1
+                pi, log_pi, _ = actor.get_action(data.observations)
+                qf_pi = qf(data.observations, pi)
+                min_qf_pi = qf_pi.min(dim=1)[0]
+                actor_loss = ((alpha * log_pi) - min_qf_pi).mean()
 
-                    if args.autotune:
-                        with torch.no_grad():
-                            _, log_pi, _ = actor.get_action(data.observations)
-                        alpha_loss = (
-                            -log_alpha.exp() * (log_pi + target_entropy)
-                        ).mean()
+                actor_optimizer.zero_grad()
+                actor_loss.backward()
+                actor_optimizer.step()
 
-                        a_optimizer.zero_grad()
-                        alpha_loss.backward()
-                        a_optimizer.step()
-                        alpha = log_alpha.exp().item()
+                if args.autotune:
+                    with torch.no_grad():
+                        _, log_pi, _ = actor.get_action(data.observations)
+                    alpha_loss = (-log_alpha.exp() * (log_pi + target_entropy)).mean()
 
-            # update the target networks
-            if global_step % args.target_network_frequency == 0:
+                    a_optimizer.zero_grad()
+                    alpha_loss.backward()
+                    a_optimizer.step()
+                    alpha = log_alpha.exp().item()
 
-                for param, target_param in zip(
-                    qf1.parameters(), qf1_target.parameters()
-                ):
-                    target_param.data.copy_(
-                        args.tau * param.data + (1 - args.tau) * target_param.data
-                    )
-                for param, target_param in zip(
-                    qf2.parameters(), qf2_target.parameters()
-                ):
-                    target_param.data.copy_(
-                        args.tau * param.data + (1 - args.tau) * target_param.data
-                    )
-
-            if global_step % 100 == 0:
-                writer.add_scalar(
-                    "losses/qf1_values", qf1_a_values.mean().item(), global_step
-                )
-                writer.add_scalar(
-                    "losses/qf2_values", qf2_a_values.mean().item(), global_step
-                )
-                writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
-                writer.add_scalar("losses/qf2_loss", qf2_loss.item(), global_step)
-                writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
-                writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
-                writer.add_scalar("losses/alpha", alpha, global_step)
-                writer.add_scalar("losses/alpha_loss", alpha_loss.item(), global_step)
-                writer.add_scalar("losses/entropy", -log_pi.mean().item(), global_step)
-
-                writer.add_scalar(
-                    "charts/SPS",
-                    int(global_step / (time.time() - start_time)),
-                    global_step,
+        # update the target networks
+        if global_step % args.target_network_frequency == 0:
+            for param, target_param in zip(qf.parameters(), qf_target.parameters()):
+                target_param.data.copy_(
+                    args.tau * param.data + (1 - args.tau) * target_param.data
                 )
 
-                logging.info(
-                    f"global_step={global_step}, actor_loss={actor_loss.item():.3f}, qf1_loss={qf1_loss.item():.3f}, qf2_loss={qf2_loss.item():.3f}, alpha={alpha:.3f}"
-                )
-                logging.info(f"SPS: {int(global_step / (time.time() - start_time))}")
-
+        if global_step % 100 == 0:
+            writer.add_scalar(
+                "charts/qf1_values", qf_a_values[:, 0].mean().item(), global_step
+            )
+            writer.add_scalar(
+                "charts/qf2_values", qf_a_values[:, 1].mean().item(), global_step
+            )
+            writer.add_scalar("losses/qf1_loss", qf1_loss.item(), global_step)
+            writer.add_scalar("losses/qf2_loss", qf2_loss.item(), global_step)
+            writer.add_scalar("losses/qf_loss", qf_loss.item() / 2.0, global_step)
+            writer.add_scalar("losses/actor_loss", actor_loss.item(), global_step)
+            writer.add_scalar("charts/alpha", alpha, global_step)
+            writer.add_scalar("losses/alpha_loss", alpha_loss.item(), global_step)
+            writer.add_scalar("charts/entropy", -log_pi.mean().item(), global_step)
+            writer.add_scalar(
+                "charts/SPS",
+                int(global_step / (time.time() - start_time)),
+                global_step,
+            )
+        if global_step % 500 == 0:
+            logging.info(
+                f"Step: {global_step}, "
+                f"QF Loss: {qf_loss.item():.3f}, "
+                f"Actor Loss: {actor_loss.item():.3f}, "
+                f"Alpha: {alpha:.3f}, "
+                f"Entropy: {-log_pi.mean().item():.3f}, "
+                f"SPS: {int(global_step / (time.time() - start_time))}"
+            )
         # Save checkpoint
-        if global_step > 0 and global_step % args.save_freq == 0:
+        if global_step % args.save_freq == 0:
             save_checkpoint(
                 actor,
-                qf1,
-                qf2,
+                qf,
                 actor_optimizer,
                 q_optimizer,
                 global_step,
@@ -396,7 +363,7 @@ def train():
             )
 
     # Final checkpoint save
-    save_checkpoint(actor, qf1, qf2, actor_optimizer, q_optimizer, global_step, args)
+    save_checkpoint(actor, qf, actor_optimizer, q_optimizer, global_step, args)
 
     env.close()
     writer.close()
@@ -412,5 +379,5 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
-    # train()
+    # test()
+    train()
